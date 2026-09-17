dbfb736b89ca68a3050b76bfc7d188271f2a6639
Roushani keshri <roushanikeshri592@gmail.com>
2026-09-17 11:16:05 +0530

    Time: 0 ms (100%), Space: 31.7 MB (89.04%) - LeetHub

---
+++ 2596-check-knight-tour-configuration/2596-check-knight-tour-configuration.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isValid(vector<vector<int>>& grid, int r, int c, int n, int expval) {
+        
+        if (r < 0 || c < 0 || r >= n || c >= n || grid[r][c] != expval) {
+            return false;
+        }
+
+        // Last cell reached
+        if (expval == n * n - 1) {
+            return true;
+        }
+
+        // 8 possible knight moves
+        bool ans1 = isValid(grid, r-2, c+1, n, expval+1);
+        bool ans2 = isValid(grid, r-1, c+2, n, expval+1);
+        bool ans3 = isValid(grid, r+1, c+2, n, expval+1);
+        bool ans4 = isValid(grid, r+2, c+1, n, expval+1);
+        bool ans5 = isValid(grid, r+2, c-1, n, expval+1);
+        bool ans6 = isValid(grid, r+1, c-2, n, expval+1);
+        bool ans7 = isValid(grid, r-1, c-2, n, expval+1);
+        bool ans8 = isValid(grid, r-2, c-1, n, expval+1);
+
+        return ans1 || ans2 || ans3 || ans4 ||
+               ans5 || ans6 || ans7 || ans8;
+    }
+
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        return isValid(grid, 0, 0, grid.size(), 0);
+    }
+};
